35cbaf233ae88910fe31c89fa85012a5a127b058
lightningfast07 <108779360+lightningfast07@users.noreply.github.com>
2023-09-23 02:18:25 +0530

    Update document_generator.jsx

--- pages/document_generator.jsx
+++ pages/document_generator.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "@/styles/components/services/LangSimpli.module.scss";
+import styles from "@/styles/components/services/docgen.module.scss";
 import Head from "next/head";
 
 function docgen() {
