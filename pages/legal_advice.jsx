0e7655b165467ae964f87843c6df7f82c379d204
Shashwat Mishra <shashwat.mishra2022@vitstudent.ac.in>
2023-09-23 01:24:11 +0530

    legal advice frontend --shash

--- pages/legal_advice.jsx
+++ pages/legal_advice.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-import styles from "@/styles/components/services/LangSimpli.module.scss";
+import { useState } from "react";
+import styles from "@/styles/components/services/legaladvice.module.scss";
 import Head from "next/head";
 import logo from "@/assets/images/logo.png";
 import Image from "next/image";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../firebaseConfig";
+import Link from "next/link";
+import { Redirect } from "next";
+import { useRouter } from 'next/router';
+// const auth = useAuth()
+//<button onClick = {(e) => auth.signinwithGoogle()}>SingIN</button>
+function SignUp() {
+    // const router = useRouter();
+    // const [email, setEmail] = useState("");
 
+    // const handleEmailChange = (event) => {
+    //     setEmail(event.target.value);
+    // };
 
-function legaladvice() {
+    // const [password, setPassword] = useState("");
+
+    // const handlePasswordChange = (event) => {
+    //     setPassword(event.target.value);
+    // };
+
+    // const handleSignUp = (e) => {
+    //     //e.preventDefault();
+
+    //     createUserWithEmailAndPassword(auth, email, password)
+    //         .then(() => {
+    //             document.getElementById("message").innerHTML = "Registration successful ,redirecting to Login...";
+    //             setTimeout(() => {
+    //                 router.push('/login');
+    //             }, 1000);
+    //         })
+    //         .catch((error) => {
+    //             document.getElementById("message").innerHTML = error.message;
+    //         });
+    // };
     return (
         <>
             <Head>
@@ -14,37 +47,103 @@ function legaladvice() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <section className={styles.section}>
+            <section className={styles.signup}>
+                <div className={styles.signup_container}>
+                    <div className={styles.signup_card_wrapper}>
+                        <div>
+                            <span className={styles.span_heading}>Legal Advice</span>
+                        </div>
+                        {/* <div className={styles.signup_ele_head}>Name</div> */}
+                        {/* <div className={styles.nameFeild}>
 
-                <div className={styles.sectionWrapper}>
-                    <div className={styles.heading} >
-                        Language Simplification
+              <input
+                className={styles.input_box}
+                placeholder="Enter first name..."
+                required
+                type="text"
+              />
+              <input
+                className={styles.input_box}
+                placeholder="Enter last name..."
+                required
+                type="text"
+              />
+            </div> */}
+                        <div className={styles.signup_ele_head}>Email</div>
+                        <input
+                            className={styles.input_box}
+                            placeholder="Enter email..."
+                            required
+                            type="email"
+                        // value={email}
+                        // onChange={handleEmailChange}
+                        />
+                        {/* <div className={styles.signup_ele_head}>User Name</div>
+            <input
+              className={styles.input_box}
+              placeholder="Enter user name..."
+              required
+            /> */}
+                        <div className={styles.signup_ele_head}>Name</div>
+                        <input
+                            className={styles.input_box}
+                            placeholder="Enter Your Name (not required).."
+                            type="text"
+                        // value={password}
+                        // onChange={handlePasswordChange}
+                        />
+                        {/* ........................................... */}
+                        <div className={styles.signup_ele_head}>Subject</div>
+                        <input
+                            className={styles.input_box}
+                            placeholder="A short description of your problem..."
+                            required
+                            type="text"
+                        // value={email}
+                        // onChange={handleEmailChange}
+                        />
+
+
+
+                        <div className={styles.signup_ele_head}>Files</div>
+                        <input
+                            className={styles.input_box}
+                            placeholder="Your documents(if required)..."
+                            type="file"
+                        // value={email}
+                        // onChange={handleEmailChange}
+                        />
+
+
+
+
+                        <div className={styles.messages} id="message"></div>
+
+
+                        {/* ............................................. */}
+                        <form
+                            className={styles.form}
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                handleSignUp();
+                            }}
+                        >
+
+
+
+
+                            <button className={styles.submit_button} type="submit">
+                                Submit
+                            </button>
+                        </form>
                     </div>
-
-                    <div className={styles.subheading1} >
-                        Original Text
+                    <div className={styles.signupImage}>
+                        <Image src={logo} alt="logo" layout="responsive" />
                     </div>
-                    <input
-                        className={styles.input_box}
-                        placeholder="Enter your text..."
-                        required
-                        type="text"
-                    />
-                    <div className={styles.subheading2} >
-                        Simplified Text
-                    </div>
-
-                    <div className={styles.result_box}>
-                        See your simplified text here...
-                    </div>
-
-
                 </div>
-
-
             </section>
         </>
     );
 }
 
-export default legaladvice;
+export default SignUp;
