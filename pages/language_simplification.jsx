c4c11e1b9a7253b02a9992bf72efc7337e0561f8
abhi <abhinavkush2003@gmail.com>
2023-09-22 22:28:35 +0530

    fixed the input boxes in the language simplification --abhi

--- pages/language_simplification.jsx
+++ pages/language_simplification.jsx
@@ -6,67 +6,63 @@ import Image from "next/image";
 
 
 function langsimpli() {
-    const [inputText, setInputText] = useState("");
-    const [simplifiedText, setSimplifiedText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [simplifiedText, setSimplifiedText] = useState("");
 
-    const handleInputChange = (event) => {
-      setInputText(event.target.value);
-    };
-    const summarizeText = async () => {
-      try {
-        const response = await fetch("/summarize", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: `input_text=${encodeURIComponent(inputText)}`,
-        });
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+  const summarizeText = async () => {
+    try {
+      const response = await fetch("/summarize", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `input_text=${encodeURIComponent(inputText)}`,
+      });
 
 
-        if (response.ok) {
-          const data = await response.json();
-          setSimplifiedText(data.Summary);
-        } else {
-          console.error("Failed to summarize text");
-        }
-      } catch (error) {
-        console.error("Error:", error);
+      if (response.ok) {
+        const data = await response.json();
+        setSimplifiedText(data.Summary);
+      } else {
+        console.error("Failed to summarize text");
       }
-    };
-    return (
-      <>
-        <Head>
-          <title>Legal.ai</title>
-          <meta name="description" content="AI based legal assistant" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-                    <div className={styles.result_box}>
-                        See your simplified text here...
-                    </div>
-                    <button className={styles.submitbtn}>SUBMIT</button>
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  return (
+    <>
+      <Head>
+        <title>Legal.ai</title>
+        <meta name="description" content="AI based legal assistant" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-                </div>
-
-
-        <section className={styles.section}>
-          <div className={styles.sectionWrapper}>
-            <div className={styles.heading}>Language Simplification</div>
-
-            <div className={styles.subheading1}>Original Text</div>
-            <input
-              className={styles.input_box}
-              placeholder="Enter your text..."
-              required
-              type="text"
-              value={inputText}
-              onChange={handleInputChange}
-            />
-            <div className={styles.subheading2}>Simplified Text</div>
-            <div className={styles.result_box}>{simplifiedText}</div>
+      <section className={styles.section}>
+        <div className={styles.sectionWrapper}>
+          <div className={styles.heading}>Language Simplification</div>
+          <div className={styles.result_box}>
+            See your simplified text here...
           </div>
-        </section>
-      </>
-    );
+          <button className={styles.submitbtn}>SUBMIT</button>
+          <div className={styles.subheading1}>Original Text</div>
+          <textarea
+            className={styles.input_box}
+            placeholder="Enter your text..."
+            required
+            type="text"
+            value={inputText}
+            onChange={handleInputChange}
+          />
+          <div className={styles.subheading2}>Simplified Text</div>
+          <div className={styles.result_box}>{simplifiedText}</div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default langsimpli;
