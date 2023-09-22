5f7d34bf995f351b1de1a63b8be6464b4de08496
Shashwat Mishra <shashwat.mishra2022@vitstudent.ac.in>
2023-09-23 00:23:23 +0530

    language translation frontend added --shash

--- pages/language_translation.jsx
+++ pages/language_translation.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import styles from "@/styles/components/services/LangSimpli.module.scss";
+import React, { useState } from "react";
+import styles from "@/styles/components/services/langtrans.module.scss";
 import Head from "next/head";
 import logo from "@/assets/images/logo.png";
 import Image from "next/image";
+// import translation from "@/pages/translation.js";
+// import countries from "@/pages/countries.js";
 
+function langsimpli() {
 
-function langtrans() {
     return (
         <>
             <Head>
@@ -15,36 +17,40 @@ function langtrans() {
             </Head>
 
             <section className={styles.section}>
-
                 <div className={styles.sectionWrapper}>
-                    <div className={styles.heading} >
-                        Language Simplification
-                    </div>
-
-                    <div className={styles.subheading1} >
-                        Original Text
-                    </div>
-                    <input
+                    <div className={styles.heading}>Language Translation</div>
+                    <textarea
+                        className={styles.result_box}
+                        placeholder="Translated text here..."
+                        required
+                        type="text"
+                        readOnly
+                        disabled
+                    // value={inputText}
+                    // onChange={handleInputChange}
+                    />
+                    <select className={styles.select2} name="" id=""></select>
+                    <button className={styles.submitbtn}>TRANSLATE</button>
+                    <div className={styles.subheading1}>Original Text</div>
+                    <textarea
+                        id="input_box"
                         className={styles.input_box}
                         placeholder="Enter your text..."
                         required
                         type="text"
+                    // value={inputText}
+                    // onChange={handleInputChange}
                     />
-                    <div className={styles.subheading2} >
-                        Simplified Text
-                    </div>
-
-                    <div className={styles.result_box}>
-                        See your simplified text here...
-                    </div>
-
-
+                    <select className={styles.select1} name="" id=""></select>
+                    <div className={styles.subheading2}>Translated Text</div>
+                    {/* <div className={styles.result_box}>{simplifiedText}</div> */}
                 </div>
-
-
             </section>
+            {/* <script src={translation} defer></script>
+            <script src={countries} defer></script> */}
         </>
     );
 }
 
-export default langtrans;
+
+export default langsimpli;
